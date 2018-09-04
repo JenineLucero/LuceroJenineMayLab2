94501b94f378a8ed244e44c2e510b83181f6314d
JenineLucero <2015081587@ust-ics.mygbiz.com>
2018-09-04 19:09:29 +0800

    Initial commit

---
+++ app/src/main/java/com/lucero/jeninemay/LuceroJenineLab2.java
@@ -0,0 +1,13 @@
+package com.lucero.jeninemay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LuceroJenineLab2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linear);
+    }
+}
